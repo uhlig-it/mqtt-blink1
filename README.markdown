aabeb3923fbed8f76463f189fdacd6243f3e1b79
Steffen Uhlig <steffen@familie-uhlig.net>
2024-01-09 20:46:27 +0100

    Make it work

--- README.markdown
+++ README.markdown
@@ -1,5 +1,22 @@
+# MQTT Blink1 Service
 
-$ rustup target add armv7-unknown-linux-gnueabihf
-$ cargo build --release --target=armv7-unknown-linux-gnueabihf
+I could not get cross-compilation on the M2 to work yet. For now, compile and run on a Raspberry Pi:
 
-  nope
+```command
+$ cargo build --release && rsync --progress -r -e ssh src/ kiosk:workspace/mqtt-blink1/src && ssh kiosk "cd workspace/mqtt-blink1 && cargo run"
+```
+
+# TODO
+
+- implement
+- find a simpler way to create the udev rules (see `go` branch)
+
+# FAQ
+
+## Why Rust?
+
+I used to run this in Ruby, but the library was never updated for Ruby 3.x. I could not fix it myself because I do not know enough about handling the native code parts.
+
+I tried go, but the cross-compilation inc. CGO gave me headaches, too.
+
+Finally, the Rust library worked without an issue.
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 use std::boxed::Box;
 use std::error::Error;
+use std::{thread, time};
 
-use blinkrs::{Blinkers, Message};
+use blinkrs::{Blinkers, Color, Message};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let blinkers: Blinkers = match Blinkers::new() {
         Ok(b) => b,
         Err(_e) => {
             println!("unable to find device");
-            return Ok(())
-        },
+            return Ok(());
+        }
     };
-    blinkers.send(Message::from("red"))?;
-    blinkers.send(Message::from("off"))?;
+
+    let interval = time::Duration::from_millis(1);
+
+    for r in (0..255).step_by(10) {
+        for g in (0..255).step_by(10) {
+            for b in (0..255).step_by(10) {
+                blinkers.send(Message::Immediate(Color::Three(r, g, b), None))?;
+                thread::sleep(interval);
+                // blinkers.send(Message::from("off"))?;
+                // thread::sleep(interval);
+            }
+        }
+    }
+
     Ok(())
 }
